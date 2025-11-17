c80e6cfa73babe1abc66c3e900642241b7c9f1f8
Nico Gulo <nicogulobelas@gmail.com>
2025-11-17 20:12:10 +0700

    feat(temprorary): refactor Home component to include Commitment and Hero components

---
+++ src/modules/Home/components/Commitment.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Commitment = () => {
+    return (
+        <div className='bg-grey-black text-grey-100 flex flex-col items-center justify-center gap-6 text-center xl:py-[160px]'>
+            <h1 className='text-heading-1-desktop'>Rarity, Quality, Collectability</h1>
+            <h3 className='paragraph-3-mobile'>Connects collectors with luxury watches from around the world</h3>
+        </div>
+    )
+}
+
+export default Commitment
