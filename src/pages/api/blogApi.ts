f6f4d2782cac7af9d1b66dabe57316f6d7a02317
Nico Gulo <nicogulobelas@gmail.com>
2025-11-19 21:53:45 +0700

    feat: add blog API integration with fetching functions for articles, categories, tags, and authors

---
+++ src/pages/api/blogApi.ts
@@ -0,0 +1,42 @@
+import axios, { AxiosRequestConfig, getAdapter } from 'axios'
+import { throttleAdapterEnhancer } from 'axios-extensions'
+
+const client = axios.create({
+    baseURL: 'https://mediumpurple-pig-833607.hostingersite.com/wp-json/wp/v2',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    adapter: throttleAdapterEnhancer(getAdapter(axios.defaults.adapter))
+})
+
+const blogFetchApi = async <T = any>(
+    config: AxiosRequestConfig
+): Promise<{ data: T; totalPages: number; totalPosts: number }> => {
+    const request = await client(config)
+    return {
+        data: request.data,
+        totalPages: Number(request.headers['x-wp-totalpages'] ?? 1),
+        totalPosts: Number(request.headers['x-wp-total'] ?? 0)
+    }
+}
+
+const clientFetchDictionary = axios.create({
+    baseURL: 'https://mediumpurple-pig-833607.hostingersite.com/wp-json/wp/v2',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    adapter: throttleAdapterEnhancer(getAdapter(axios.defaults.adapter))
+})
+
+export const blogDictionaryFetchApi = async <T = any>(
+    config: AxiosRequestConfig
+): Promise<{ data: T; totalPages: number; totalPosts: number }> => {
+    const request = await clientFetchDictionary(config)
+    return {
+        data: request.data,
+        totalPages: Number(request.headers['x-wp-totalpages'] ?? 1),
+        totalPosts: Number(request.headers['x-wp-total'] ?? 0)
+    }
+}
+
+export default blogFetchApi
