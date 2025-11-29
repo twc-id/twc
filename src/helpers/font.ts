9c3fab044d9636928054d6167fb49c367a6b6242
Nico Gulo <nicogulobelas@gmail.com>
2025-11-29 12:46:43 +0700

    fix: font override

--- src/helpers/font.ts
+++ src/helpers/font.ts
@@ -1,9 +1,17 @@
 /* eslint-disable import/prefer-default-export */
-import { Inter } from 'next/font/google'
+import { Inter, Overpass } from 'next/font/google'
 
 export const inter = Inter({
     variable: '--font-inter',
     weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
+    style: ['normal'],
+    subsets: ['latin'],
+    display: 'swap'
+})
+
+export const overpass = Overpass({
+    variable: '--font-overpass',
+    weight: ['300', '400', '500', '600', '700', '800'],
     style: ['normal'],
     subsets: ['latin'],
     display: 'swap'
