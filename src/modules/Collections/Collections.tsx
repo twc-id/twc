8d90d6e6b5fc3c5a315ae02bac957f931733a10a
Nico Gulo <nicogulobelas@gmail.com>
2025-12-18 08:51:56 +0700

    feat: implement Hero component in Collections module with GSAP animations

--- src/modules/Collections/Collections.tsx
+++ src/modules/Collections/Collections.tsx
@@ -1,7 +1,14 @@
+import Seo from '@components/Seo'
+import Hero from '@modules/Collections/components/Hero'
 import React from 'react'
 
 const Collections = () => {
-    return <div>Collections</div>
+    return (
+        <>
+            <Seo title='Collections' />
+            <Hero />
+        </>
+    )
 }
 
 export default Collections
