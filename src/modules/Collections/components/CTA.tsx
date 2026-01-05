96c5a3171fe75a0caae176e3a75ff3d9f7c16dd9
Nico Gulo <nicogulobelas@gmail.com>
2026-01-05 20:21:30 +0700

    feat: update CTA components for responsive text styling across devices

--- src/modules/Collections/components/CTA.tsx
+++ src/modules/Collections/components/CTA.tsx
@@ -39,16 +39,22 @@ const CTA = () => {
         }
     }, [])
     return (
-        <section className='bg-grey-white py-14 xl:py-[116px]' ref={sectionRef}>
+        <section className='bg-grey-white py-6 xl:py-[116px]' ref={sectionRef}>
             <Container>
                 <div className='bg-grey-black relative flex h-full w-full flex-col items-center gap-14 overflow-hidden xl:flex-row xl:justify-between'>
                     <div className='flex w-full flex-col items-start gap-5 px-5 pt-8 xl:max-w-[442px] xl:gap-6 xl:pl-20 xl:pr-0'>
-                        <h2 className='text-heading-2-desktop text-grey-white' ref={headingref}>
+                        <h2
+                            className='xl:text-heading-2-desktop text-heading-2-mobile text-grey-white'
+                            ref={headingref}
+                        >
                             <Trans i18nKey='cta.title' components={{ br: <br /> }}>
                                 {t('cta.title')}
                             </Trans>
                         </h2>
-                        <p className='text-paragraph-6-desktop text-grey-100' ref={paragraphRef}>
+                        <p
+                            className='xl:text-paragraph-6-desktop text-paragraph-6-mobile text-grey-100'
+                            ref={paragraphRef}
+                        >
                             {t('cta.description')}
                         </p>
                         <a
