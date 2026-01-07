5b2eacbd14626cad207c2c7c072c52e9b8601e97
Nico Gulo <nicogulobelas@gmail.com>
2026-01-07 22:12:45 +0700

    feat: enhance components with related articles and improved sanitization

--- src/modules/Article/ArticleDetailPage.tsx
+++ src/modules/Article/ArticleDetailPage.tsx
@@ -1,6 +1,10 @@
+import Container from '@components/Container'
 import Seo from '@components/Seo'
-import { Article as ArticleType } from '@hooks/useArticle'
+import { Article as ArticleType, useRelatedArticles } from '@hooks/useArticle'
+import ArticleCTA from '@modules/Article/components/ArticleCTA'
+import RelatedArticles from '@modules/Article/components/RelatedArticles'
 import React from 'react'
+import { When } from 'react-if'
 
 import ArticleDetail from './components/ArticleDetail'
 
@@ -9,10 +13,21 @@ interface ArticleDetailPageProps {
 }
 
 const ArticleDetailPage: React.FC<ArticleDetailPageProps> = ({ article }) => {
+    const { data: relatedArticles } = useRelatedArticles(
+        article.id,
+        article._embedded?.['wp:term']?.[0]?.map((term) => term.id) || []
+    )
+
     return (
         <>
             <Seo title={article.title.rendered} description={article.uagb_excerpt} date={article.date} />
             <ArticleDetail article={article} />
+            <When condition={relatedArticles && relatedArticles.data.length > 0}>
+                <RelatedArticles article={relatedArticles} />
+            </When>
+            <Container className='pb-14 xl:pb-[116px]'>
+                <ArticleCTA />
+            </Container>
         </>
     )
 }
--- src/modules/Article/components/ArticleCard.tsx
+++ src/modules/Article/components/ArticleCard.tsx
@@ -1,21 +1,25 @@
 import UnstyledLink from '@components/links/UnstyledLink'
 import { Article } from '@hooks/useArticle'
+import classNames from '@lib/classnames'
 import { formatDate } from '@utils/format-date'
+import { sanitize } from 'isomorphic-dompurify'
 import Image from 'next/image'
 import React from 'react'
 
 interface ArticleCardProps {
     article: Article
+    titleClassName?: string
+    metaClassName?: string
 }
 
-const ArticleCard: React.FC<ArticleCardProps> = ({ article }) => {
+const ArticleCard: React.FC<ArticleCardProps> = ({ article, titleClassName, metaClassName }) => {
     const featuredImage = article._embedded?.['wp:featuredmedia']?.[0]?.source_url
 
-    const tags = article._embedded?.['wp:term']?.[1] || []
+    const category = article._embedded?.['wp:term']?.[0] || []
     const estimateReadingTime = article.reading_time?.minutes
     return (
         <UnstyledLink href={`/articles/${article.slug}`} className='group block'>
-            <div className='overflow-hidden bg-white'>
+            <div className='overflow-hidden '>
                 {/* Featured Image */}
                 {featuredImage && (
                     <div className='relative min-h-[270px] w-full overflow-hidden xl:max-w-[405px]'>
@@ -29,32 +33,37 @@ const ArticleCard: React.FC<ArticleCardProps> = ({ article }) => {
                 )}
 
                 {/* Content */}
-                <div className='flex flex-col gap-4 xl:pt-6'>
+                <div className='flex flex-col gap-3 pt-5 xl:pt-6'>
                     {/* Title */}
                     <h3
-                        className='group-hover:text-primary-500 xl:text-button-1-desktop text-button-1-mobile line-clamp-2 font-semibold text-gray-900'
-                        dangerouslySetInnerHTML={{ __html: article.title.rendered }}
+                        className={classNames(
+                            'group-hover:text-primary-500 xl:text-button-1-desktop text-button-1-mobile line-clamp-2 font-semibold text-gray-900',
+                            titleClassName
+                        )}
+                        dangerouslySetInnerHTML={{ __html: sanitize(article.title.rendered) }}
                     />
 
                     {/* Meta */}
-                    <div className='flex items-center gap-2 '>
+                    <div className={classNames('flex items-center gap-2', metaClassName)}>
                         {/* Tags */}
-                        {tags.length > 0 &&
-                            tags.slice(0, 1).map((tag: any) => (
-                                <span
-                                    key={tag.id}
-                                    className='border-grey-500 xl:text-paragraph-9-desktop text-paragraph-9-mobile rounded-full border-[0.5px] px-3 py-1 !leading-none text-gray-500'
-                                >
-                                    {tag.name}
-                                </span>
-                            ))}
+                        {category.length > 0 &&
+                            category
+                                .slice(0, 1)
+                                .map((categories: any) => (
+                                    <span
+                                        key={categories.id}
+                                        className='border-grey-500 xl:text-paragraph-9-desktop text-paragraph-9-mobile rounded-full border-[0.5px] px-3 py-1 !leading-none text-gray-500'
+                                        dangerouslySetInnerHTML={{ __html: sanitize(categories.name) }}
+                                    />
+                                ))}
 
                         <div className='flex flex-row gap-2'>
-                            <span className='xl:text-paragraph-7-desktop text-paragraph-7-mobile !leading-none text-gray-500'>
+                            <span className='xl:text-paragraph-7-desktop text-paragraph-7-mobile  text-gray-500'>
                                 {formatDate(article.date, 'MMM, YYYY')}
                             </span>
-                            <div className='border-r-grey-500 border' />
-                            <span className='xl:text-paragraph-7-desktop text-paragraph-7-mobile !leading-none text-gray-500'>
+
+                            <div className='bg-grey-500 mb-1 h-[13px] w-[1px]' />
+                            <span className='xl:text-paragraph-7-desktop text-paragraph-7-mobile  text-gray-500'>
                                 {estimateReadingTime} min read
                             </span>
                         </div>
