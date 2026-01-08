8f38cf88a5a7f928840c00c4ce860c6393eebe4f
Nico Gulo <nicogulobelas@gmail.com>
2026-01-08 20:52:09 +0700

    fix: remove custom styling header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -165,24 +165,6 @@ const PAGE_STYLES: Record<string, PageStyle> = {
             menuOpen: 'LogoWhite',
             searchOpen: 'LogoBlack'
         }
-    },
-    '/privacy-policy': {
-        navbar: {
-            default: 'bg-transparent',
-            scrolled: 'bg-[#0F0F0FCC] backdrop-blur-[20px]'
-        },
-        icons: {
-            default: 'text-grey-200',
-            scrolled: 'text-black',
-            menuOpen: 'text-grey-200',
-            searchOpen: 'text-black'
-        },
-        logo: {
-            default: 'LogoBlack',
-            scrolled: 'LogoBlack',
-            menuOpen: 'LogoWhite',
-            searchOpen: 'LogoBlack'
-        }
     }
 }
 
