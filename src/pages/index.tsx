fbdf482b2a36146d1bcf9af7e83a6f7598a48392
nicogulo <nicogulobelas@gmail.com>
2026-01-10 15:37:30 +0700

    fix: update getStaticProps to getServerSideProps for server-side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import { defaultLanguage } from '@constant/i18n'
 import Home from '@modules/Home'
-import { GetStaticProps } from 'next'
+import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
-export const getStaticProps: GetStaticProps = async ({ locale }) => ({
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => ({
     props: {
         ...(await serverSideTranslations(locale || defaultLanguage, ['components', 'common', 'pages', 'home']))
-    },
-    revalidate: 600
+    }
 })
 
 export default Home
