564501992a4bee22ddf767aeb433ac1631bded3c
nicogulo <nicogulobelas@gmail.com>
2026-01-28 22:49:15 +0700

    feat: update GA_EVENTS for enhanced Google Analytics tracking

--- src/lib/constants/analyticsEvents.ts
+++ src/lib/constants/analyticsEvents.ts
@@ -1,5 +1,12 @@
 // Event names for Google Analytics trackingh
 export const GA_EVENTS = {
-    SEARCH: 'search',
-    VIEW_MORE: 'view_more'
+    SEARCH: 'interest_search',
+    INTEREST_WATCHES: 'interest_watches',
+    INTEREST_ACCESSORIES: 'interest_accessories',
+    INTEREST_WATCH_DETAILS: 'interest_watch_details',
+    INTEREST_ACCESSORIES_DETAILS: 'interest_accessories_details',
+    CONTACT_WA: 'contact_wa',
+    INTEREST_INSTAGRAM: 'interest_instagram',
+    FILTER_SELECTED: 'filter_selected',
+    SUBSCRIBE_NEWSLETTER: 'subscribe_newsletter'
 } as const
