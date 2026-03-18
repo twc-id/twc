a78414d3f74ca275ca7edf759feeeb06a85a2584
Nico Gulo <nicogulobelas@gmail.com>
2026-03-18 17:19:13 +0700

    fix: update font

--- src/tailwind-config/font-sizes.config.js
+++ src/tailwind-config/font-sizes.config.js
@@ -14,20 +14,29 @@ const fontSize = {
 
     // Desktop
     'button-1-desktop': [
-        '28px',
-        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 400, fontFamily: 'Inter' }
+        '24px',
+        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 300, fontFamily: 'Inter' }
     ],
-    'button-2-desktop': ['16px', { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 500, fontFamily: 'Inter' }],
-    'button-3-desktop': ['16px', { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 400, fontFamily: 'Inter' }],
-    'button-4-desktop': ['14px', { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 600, fontFamily: 'Inter' }],
-    'button-5-desktop': ['14px', { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 500, fontFamily: 'Inter' }],
+    'button-2-desktop': ['14px', { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 400, fontFamily: 'Inter' }],
+    'button-3-desktop': [
+        '14px',
+        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 300, fontFamily: 'Inter' }
+    ],
+    'button-4-desktop': [
+        '13px',
+        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 500, fontFamily: 'Inter' }
+    ],
+    'button-5-desktop': [
+        '13px',
+        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 400, fontFamily: 'Inter' }
+    ],
 
     // Mobile
-    'button-1-mobile': ['22px', { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 400, fontFamily: 'Inter' }],
-    'button-2-mobile': ['14px', { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 500, fontFamily: 'Inter' }],
-    'button-3-mobile': ['14px', { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 400, fontFamily: 'Inter' }],
-    'button-4-mobile': ['13px', { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 600, fontFamily: 'Inter' }],
-    'button-5-mobile': ['13px', { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 500, fontFamily: 'Inter' }],
+    'button-1-mobile': ['22px', { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 300, fontFamily: 'Inter' }],
+    'button-2-mobile': ['14px', { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 400, fontFamily: 'Inter' }],
+    'button-3-mobile': ['14px', { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 300, fontFamily: 'Inter' }],
+    'button-4-mobile': ['13px', { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 300, fontFamily: 'Inter' }],
+    'button-5-mobile': ['13px', { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 400, fontFamily: 'Inter' }],
 
     // ==============================
     // HEADINGS
@@ -44,15 +53,15 @@ const fontSize = {
     ],
     'heading-1-3-desktop': [
         '56px',
-        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 300, fontFamily: 'Inter' }
+        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 400, fontFamily: 'Inter' }
     ],
     'heading-2-desktop': [
-        '49px',
-        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 300, fontFamily: 'Inter' }
+        '48px',
+        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 400, fontFamily: 'Inter' }
     ],
     'heading-3-desktop': [
         '32px',
-        { lineHeight: '130%', letterSpacing: '-0.04em', fontWeight: 300, fontFamily: 'Inter' }
+        { lineHeight: '130%', letterSpacing: '-0.04em', fontWeight: 400, fontFamily: 'Inter' }
     ],
 
     // Mobile
@@ -83,62 +92,62 @@ const fontSize = {
 
     // Desktop
     'subheading-1-desktop': [
-        '32px',
-        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 500, fontFamily: 'Inter' }
-    ],
-    'subheading-2-desktop': [
-        '20px',
-        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 500, fontFamily: 'Inter' }
-    ],
-    'subheading-3-desktop': [
-        '20px',
+        '28px',
         { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 400, fontFamily: 'Inter' }
     ],
-    'subheading-4-desktop': [
+    'subheading-2-desktop': [
         '18px',
-        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 500, fontFamily: 'Inter' }
+        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 400, fontFamily: 'Inter' }
+    ],
+    'subheading-3-desktop': [
+        '18px',
+        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 300, fontFamily: 'Inter' }
+    ],
+    'subheading-4-desktop': [
+        '16px',
+        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 400, fontFamily: 'Inter' }
     ],
     'subheading-5-desktop': [
-        '16px',
-        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 500, fontFamily: 'Inter' }
+        '14px',
+        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 400, fontFamily: 'Inter' }
     ],
     'subheading-6-desktop': [
-        '14px',
+        '13px',
         { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 600, fontFamily: 'Inter' }
     ],
     'subheading-7-desktop': [
         '12px',
-        { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 400, fontFamily: 'Inter' }
+        { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 300, fontFamily: 'Inter' }
     ],
 
     // Mobile
     'subheading-1-mobile': [
         '22px',
-        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 500, fontFamily: 'Inter' }
+        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 400, fontFamily: 'Inter' }
     ],
     'subheading-2-mobile': [
         '16px',
-        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 500, fontFamily: 'Inter' }
+        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 400, fontFamily: 'Inter' }
     ],
     'subheading-3-mobile': [
         '16px',
-        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 400, fontFamily: 'Inter' }
+        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 300, fontFamily: 'Inter' }
     ],
     'subheading-4-mobile': [
         '15px',
-        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 500, fontFamily: 'Inter' }
+        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 400, fontFamily: 'Inter' }
     ],
     'subheading-5-mobile': [
         '14px',
-        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 500, fontFamily: 'Inter' }
+        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 400, fontFamily: 'Inter' }
     ],
     'subheading-6-mobile': [
         '13px',
-        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 600, fontFamily: 'Inter' }
+        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 500, fontFamily: 'Inter' }
     ],
     'subheading-7-mobile': [
         '11px',
-        { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 400, fontFamily: 'Inter' }
+        { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 300, fontFamily: 'Inter' }
     ],
 
     // ==============================
@@ -147,24 +156,24 @@ const fontSize = {
 
     // Desktop
     'paragraph-1-desktop': [
-        '40px',
-        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 400, fontFamily: 'Inter' }
+        '36px',
+        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 300, fontFamily: 'Inter' }
     ],
     'paragraph-2-desktop': [
-        '36px',
-        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 400, fontFamily: 'Inter' }
+        '32px',
+        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 300, fontFamily: 'Inter' }
     ],
     'paragraph-3-desktop': [
-        '24px',
-        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 500, fontFamily: 'Inter' }
+        '20px',
+        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 400, fontFamily: 'Inter' }
     ],
     'paragraph-4-desktop': [
-        '18px',
-        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 600, fontFamily: 'Overpass' }
+        '20px',
+        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 400, fontFamily: 'Inter' }
     ],
     'paragraph-5-desktop': [
-        '18px',
-        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 400, fontFamily: 'Overpass' }
+        '16x',
+        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 600, fontFamily: 'Overpass' }
     ],
     'paragraph-6-desktop': [
         '16px',
@@ -175,18 +184,22 @@ const fontSize = {
         { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 400, fontFamily: 'Overpass' }
     ],
     'paragraph-8-desktop': [
-        '12px',
-        { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 500, fontFamily: 'Overpass' }
+        '13px',
+        { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 400, fontFamily: 'Overpass' }
     ],
     'paragraph-9-desktop': [
         '12px',
-        { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 400, fontFamily: 'Overpass' }
+        { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 500, fontFamily: 'Overpass' }
     ],
     'paragraph-10-desktop': [
         '12px',
-        { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 300, fontFamily: 'Overpass' }
+        { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 400, fontFamily: 'Overpass' }
     ],
     'paragraph-11-desktop': [
+        '12px',
+        { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 300, fontFamily: 'Overpass' }
+    ],
+    'paragraph-12-desktop': [
         '12px',
         { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 300, fontFamily: 'Overpass' }
     ],
@@ -194,45 +207,49 @@ const fontSize = {
     // Mobile
     'paragraph-1-mobile': [
         '24px',
-        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 400, fontFamily: 'Inter' }
+        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 300, fontFamily: 'Inter' }
     ],
     'paragraph-2-mobile': [
         '24px',
-        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 400, fontFamily: 'Inter' }
+        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 300, fontFamily: 'Inter' }
     ],
     'paragraph-3-mobile': [
         '18px',
-        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 500, fontFamily: 'Inter' }
+        { lineHeight: '110%', letterSpacing: '-0.04em', fontWeight: 400, fontFamily: 'Inter' }
     ],
     'paragraph-4-mobile': [
-        '15px',
-        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 600, fontFamily: 'Overpass' }
+        '18px',
+        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 500, fontFamily: 'Inter' }
     ],
     'paragraph-5-mobile': [
         '15px',
-        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 400, fontFamily: 'Overpass' }
+        { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 600, fontFamily: 'Overpass' }
     ],
     'paragraph-6-mobile': [
-        '14px',
+        '15px',
         { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 400, fontFamily: 'Overpass' }
     ],
     'paragraph-7-mobile': [
-        '13px',
+        '14px',
         { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 400, fontFamily: 'Overpass' }
     ],
     'paragraph-8-mobile': [
-        '11px',
-        { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 500, fontFamily: 'Overpass' }
+        '13px',
+        { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 400, fontFamily: 'Overpass' }
     ],
     'paragraph-9-mobile': [
         '11px',
-        { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 400, fontFamily: 'Overpass' }
+        { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 500, fontFamily: 'Overpass' }
     ],
     'paragraph-10-mobile': [
         '11px',
-        { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 300, fontFamily: 'Overpass' }
+        { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 400, fontFamily: 'Overpass' }
     ],
     'paragraph-11-mobile': [
+        '11px',
+        { lineHeight: '130%', letterSpacing: '0.02em', fontWeight: 300, fontFamily: 'Overpass' }
+    ],
+    'paragraph-12-mobile': [
         '11px',
         { lineHeight: '130%', letterSpacing: '-0.02em', fontWeight: 300, fontFamily: 'Overpass' }
     ]
