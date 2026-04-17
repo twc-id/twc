fa183e4e5adb36ddc4e0719a74e5d88cb05a2433
Nico Gulo <nicogulobelas@gmail.com>
2026-04-17 20:56:21 +0700

    fix: svg formating

--- src/components/Icon/Images/ChartGreen.tsx
+++ src/components/Icon/Images/ChartGreen.tsx
@@ -4,8 +4,8 @@ import * as React from 'react'
 const ChartGreen: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
     <svg width='14' height='16' viewBox='0 0 14 16' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
         <path
-            fill-rule='evenodd'
-            clip-rule='evenodd'
+            fillRule='evenodd'
+            clipRule='evenodd'
             d='M4.14998 0.850006H2.84998V2.85001H0.849976V13.15H2.84998V15.15H4.14998V13.15H6.14998V2.85001H4.14998V0.850006ZM11.15 0.850006H9.84998V4.65801H7.84998V10.535H9.84998V15.15H11.15V10.535H13.15V4.65801H11.15V0.850006ZM2.14998 11.85V4.15001H4.84998V11.85H2.14998ZM9.14997 9.23401V5.95801H11.85V9.23401H9.14997Z'
             fill='currentColor'
         />
--- src/components/Icon/Images/IndoFlag.tsx
+++ src/components/Icon/Images/IndoFlag.tsx
@@ -3,9 +3,9 @@ import * as React from 'react'
 
 const IndoFlag: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
     <svg width='20' height='15' viewBox='0 0 20 15' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
-        <g clip-path='url(#clip0_3018_10329)'>
+        <g clipPath='url(#clip0_3018_10329)'>
             <rect width='20' height='15' fill='white' />
-            <path fill-rule='evenodd' clip-rule='evenodd' d='M0 0V15H20V0H0Z' fill='#E9E9E9' />
+            <path fillRule='evenodd' clipRule='evenodd' d='M0 0V15H20V0H0Z' fill='#E9E9E9' />
             <mask
                 id='mask0_3018_10329'
                 style={{ maskType: 'luminance' }}
@@ -15,10 +15,10 @@ const IndoFlag: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
                 width='20'
                 height='15'
             >
-                <path fill-rule='evenodd' clip-rule='evenodd' d='M0 0V15H20V0H0Z' fill='white' />
+                <path fillRule='evenodd' clipRule='evenodd' d='M0 0V15H20V0H0Z' fill='white' />
             </mask>
             <g mask='url(#mask0_3018_10329)'>
-                <path fill-rule='evenodd' clip-rule='evenodd' d='M0 0V7.5H20V0H0Z' fill='#E31D1C' />
+                <path fillRule='evenodd' clipRule='evenodd' d='M0 0V7.5H20V0H0Z' fill='#E31D1C' />
             </g>
         </g>
         <defs>
